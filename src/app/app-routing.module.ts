9cf1219930307b9620fa8e8443fdf1ed80e0c42f
marwen <eshilimarwen9@gmail.com>
2021-06-11 19:51:50 +0200

    fix backgrounds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
+import { CardsComponent } from './cards/cards.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: 'cantact', component:ContactComponent},
   {path: 'projects', component:ProjectsComponent},
   {path: 'skills', component:SkillsComponent},
+  {path: 'cards', component:CardsComponent},
+
 
   
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,32 @@ import { MaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SkillsComponent } from './skills/skills.component';
+import { CardsComponent } from './cards/cards.component';
+import { ProfileComponent } from './home/profile/profile.component';
+import { KnowledgeComponent } from './home/knowledge/knowledge.component';
+import { EducationsComponent } from './home/educations/educations.component';
+import { WorksComponent } from './home/works/works.component';
+import { AboutmeComponent } from './home/aboutme/aboutme.component';
+import { ContactComponent } from './home/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ContactComponent,
     HomeComponent,
     ProjectsComponent,
-    SkillsComponent
+    SkillsComponent,
+    CardsComponent,
+    ProfileComponent,
+    KnowledgeComponent,
+    EducationsComponent,
+    WorksComponent,
+    AboutmeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
