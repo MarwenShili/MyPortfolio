87a4179426f63786c9fad6ff414aea83bbbadbfe
marwen <eshilimarwen9@gmail.com>
2021-07-08 20:45:49 +0200

    profile modification + front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material-module';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,11 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SkillsComponent } from './skills/skills.component';
 import { CardsComponent } from './cards/cards.component';
 import { ProfileComponent } from './home/profile/profile.component';
-import { KnowledgeComponent } from './home/knowledge/knowledge.component';
-import { EducationsComponent } from './home/educations/educations.component';
-import { WorksComponent } from './home/works/works.component';
-import { AboutmeComponent } from './home/aboutme/aboutme.component';
-import { ContactComponent } from './home/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -27,11 +21,6 @@ import { ContactComponent } from './home/contact/contact.component';
     SkillsComponent,
     CardsComponent,
     ProfileComponent,
-    KnowledgeComponent,
-    EducationsComponent,
-    WorksComponent,
-    AboutmeComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
